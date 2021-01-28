9761fbced15bea9a8652cf46466f584a07f1f34f
JoaquinManchenoP <manchenojj@g.cofc.edu>
2021-01-28 17:51:17 -0500

    first commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+
+
+}
